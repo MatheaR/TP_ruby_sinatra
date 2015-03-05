00d5375985f7a27efde16d71aafa609bad59385a
florianlaubier <florian.laubier@ingesup.com>
2015-03-05 14:59:14 +0100

    retour vers le futur

--- app.rb
+++ app.rb
@@ -7,76 +7,74 @@ require_relative 'models/message'
 
 enable :sessions
 
-class Chat < Sinatra::Base
-  configure do
-      Mongoid.load!("mongoid.yml")
-      mime_type :css, 'text/css'
-  end
+configure do
+    Mongoid.load!("mongoid.yml")
+    mime_type :css, 'text/css'
+end
 
-  get '/' do
-    erb :index
-  end
+get '/' do
+  erb :index
+end
 
-  post '/' do
-    erb :index
-  end
+post '/' do
+  erb :index
+end
 
-  get '/tchat' do
-    if(!session[:id].nil? && !session[:user].nil?)
-      @userId = session[:id]
-      @userSession = session[:user]
-      @messages = Message.all
-      @title = "Viens tchatter !"
-      erb :tchat
+get '/tchat' do
+  if(!session[:id].nil? && !session[:user].nil?)
+    @userId = session[:id]
+    @userSession = session[:user]
+    @messages = Message.all
+    @title = "Viens tchatter !"
+    erb :tchat
+  else
+    redirect to("/haha")
+  end
+end
+
+get '/haha' do
+  erb :haha
+end
+
+post '/login' do
+    if User.where(pseudo: params[:user]).exists?
+      @a = User.find_by(pseudo: params[:user])
+      session[:id] = @a.id
+      session[:user] = @a.pseudo
+      redirect to("/tchat")
     else
-      redirect to("/haha")
-    end
-  end
-
-  get '/haha' do
-    erb :haha
-  end
-
-  post '/login' do
-      if User.where(pseudo: params[:user]).exists?
-        @a = User.find_by(pseudo: params[:user])
-        session[:id] = @a.id
-        session[:user] = @a.pseudo
+      # ifparams.has_key?(:user) && params.has_key?(:initiale)
+      if params[:user].present? && params[:initiale].present?
+        @user = User.create(pseudo: params[:user], initiale: params[:initiale])
+        puts "Successfully registered"
+         @b = User.find_by(pseudo: params[:user])
+        session[:id] = @b.id
+        session[:user] = @b.pseudo
         redirect to("/tchat")
       else
-        # ifparams.has_key?(:user) && params.has_key?(:initiale)
-        if params[:user].present? && params[:initiale].present?
-          @user = User.create(pseudo: params[:user], initiale: params[:initiale])
-          puts "Successfully registered"
-           @b = User.find_by(pseudo: params[:user])
-          session[:id] = @b.id
-          session[:user] = @b.pseudo
-          redirect to("/tchat")
-        else
-          puts params[:user]
-          puts params[:initiale]
-          # redirect to("/haha")
-        end
+        puts params[:user]
+        puts params[:initiale]
+        # redirect to("/haha")
       end
-  end
+    end
+end
 
-  post '/goMess' do
-      @mess = Message.create(params[:message])
-      redirect to("/tchat")
-  end
+post '/goMess' do
+    @mess = Message.create(params[:message])
+    redirect to("/tchat")
+end
 
-  post '/deco' do
-      [:user, :id].each { |k| session.delete(k) }
-      redirect to("/")
-  end
+post '/deco' do
+    [:user, :id].each { |k| session.delete(k) }
+    redirect to("/")
+end
 
-  post '/supAll' do
-      @supAll = Message.all_in(:user_id => [params[:user_id]]).destroy_all
-      redirect to("/tchat")
-  end
+post '/supAll' do
+    @supAll = Message.all_in(:user_id => [params[:user_id]]).destroy_all
+    redirect to("/tchat")
+end
 
-  post '/supOne' do
-      @supOne = Message.any_in(:_id => [params[:id]]).destroy_all
-      redirect to("/tchat")
-  end
+post '/supOne' do
+    @supOne = Message.any_in(:_id => [params[:id]]).destroy_all
+    redirect to("/tchat")
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
 require './app'
-run Chat.new
+run Sinatra::Application
